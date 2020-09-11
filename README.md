60695d5f2e6a055bff7aec43a5ae7f6baf8109e2
Nicolas Brugneaux <nicolas@passbase.com>
2020-09-11 14:24:44 +0200

    docs: add README.md

---
+++ README.md
@@ -0,0 +1,18 @@
+# Banner Generator for Passbase SDKs [![Netlify Status](https://api.netlify.com/api/v1/badges/e6737d7b-1a40-4226-9db0-a25cc11b63a7/deploy-status)](https://app.netlify.com/sites/passbase-sdk-banner/deploys)
+
+### How to develop
+
+- Requirements: Node LTS
+- `npm install`
+
+Then either run
+
+- `npm run build` to generate all banners
+
+or
+
+- `node ./cli.js [lang] [outDir]` to generate a single banner
+
+### Deployment
+
+Auto deployed via netfliy at https://passbase-sdk-banner.netlify.app
--- index.js
+++ index.js
@@ -2,41 +2,39 @@ const { join } = require("path");
 const { readdirSync, writeFileSync, readFileSync } = require("fs");
 const { fork } = require("child_process");
 
-const availableLanguages = readdirSync(join(__dirname, "logos"))
+const LANGUAGES = readdirSync(join(__dirname, "logos"))
   .map((fileName) => fileName.split(".")[0])
   .filter(Boolean);
 
-const processes = availableLanguages
-  .map((lang) => {
-    return fork(join(__dirname, "cli.js"), ["generate", lang, "public"], {
-      stdio: [null, process.stdout, process.stdout, "ipc"],
-    });
-  })
-  .filter(Boolean);
+function exportHtml() {
+  const html = readFileSync(join("assets", "index.html")).toString();
+
+  writeFileSync(
+    join("public", "index.html"),
+    Buffer.from(
+      html.replace(
+        "{links}",
+        LANGUAGES.map(
+          (lang) => `    <a href="/${lang}.png"><img src="/${lang}.png"></a>`
+        ).join("\n")
+      )
+    )
+  );
+}
+
+const processes = LANGUAGES.map((lang) => {
+  return fork(join(__dirname, "cli.js"), ["generate", lang, "public"], {
+    stdio: [null, process.stdout, process.stdout, "ipc"],
+  });
+}).filter(Boolean);
 
 let exited = 0;
 processes.forEach((child) =>
   child.on("exit", () => {
     exited += 1;
     if (exited === processes.length) {
-      const html = readFileSync(join("assets", "index.html")).toString();
-
-      writeFileSync(
-        join("public", "index.html"),
-        Buffer.from(
-          html.replace(
-            "{links}",
-            availableLanguages
-              .map(
-                (lang) =>
-                  `    <a href="/${lang}.png"><img src="/${lang}.png"></a>`
-              )
-              .join("\n")
-          )
-        )
-      );
-
-      console.log(`Generated banners for ${availableLanguages.join(", ")}!`);
+      exportHtml();
+      console.log(`Generated banners for ${LANGUAGES.join(", ")}!`);
       process.exit();
     }
   })
